a5c9c554e6cd603fd892d752561d8c59ca601a4d
二白克大鹅 <2891018500@qq.com>
2025-01-09 11:49:41 +0800

    idea git second commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("hot-fix change1");
+        System.out.println("合并冲突master");
     }
 }
